e77397a2d72778c6d46170c753194229905a7dd5
Agustin Ritter <103846339+Agusr1997@users.noreply.github.com>
2023-03-08 19:45:18 -0300

    Add files via upload

---
+++ hospital_consultorio.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hospital
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `consultorio`
+--
+
+LOCK TABLES `consultorio` WRITE;
+/*!40000 ALTER TABLE `consultorio` DISABLE KEYS */;
+INSERT INTO `consultorio` VALUES (1,'Hospital Roca','Avenida Montevideo 168',1122556688,'hospRoca@Outlook.com'),(2,'Hospital Flores','Avenida Los Pasos 115',35489967,'hospFlores@Outlook.com'),(3,'Hospital Cuidados Graves','Avenida Hidalgo 487',1145779368,'hospCuidadosGraves@Outlook.com'),(4,'Hospital Jose San Martin','Triunbirato 500',1144751986,'hospJoseSanMartin@Outlook.com'),(5,'Hospital Duran','Vera 100',1144778844,'hospDuran@Outlook.com');
+/*!40000 ALTER TABLE `consultorio` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-08 19:43:52
